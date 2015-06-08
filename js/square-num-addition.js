45211b22f9a8395b91b5ef2585d282f59b261f0d
Judah Lamm <Judah.Lamm@gmail.com>
2015-06-08 10:01:50 -1000

    Updates to the solution for the challenge

--- js/square-num-addition.js
+++ js/square-num-addition.js
@@ -3,10 +3,11 @@ var squareFunc = function(inputNum){
   var squareNum = 0;
   var totals = 0;
 
+  if((typeof inputNum !== 'number')) return false;
+
   for(var i = 1; i <= inputNum; i++){
 
     squareNum = Math.sqrt(i);
-
 
     totals+=squareNum;
   }
--- test/square-num-addition-spec.js
+++ test/square-num-addition-spec.js
@@ -6,19 +6,15 @@ describe('Square Root Sum', function () {
   it('squareFunc should be a function', function () {
     squares.squareFunc.should.be.a('function');
   });
-
   it('The input should be a number', function () {
     var result = squares.squareFunc(9)
     result.should.be.a('number');
   });
   it('function should take the square root of the input and sum from 1 to that num', function () {
-    var result = squares.squareFunc(9)
-
-    result.should.equal(6);
+    var result = squares.squareFunc(3)
+    result.should.equal(4.15);
   });
-  it('should return false from prime numbers', function () {
-    var result = squares.squareFunc(7)
-    result.should.be.false;
-
+  it('should return false for not number', function () {
+    squares.squareFunc('beans').should.be.false;
   });
 });
